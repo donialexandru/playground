93a8b795a367b989fafbb9a48e4b0db167b84703
Alexandru Doni <donialexandru@gmail.com>
2025-02-27 10:04:04 +0200

    add elements on the page

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
+const grandParent = document.getElementById("grand-parent");
+const parent = document.getElementById("parent");
 const child = document.getElementById("child");
 
-child.addEventListener("click", () => {
-  console.log("you pressed on first child");
+grandParent.addEventListener("click", (e) => {
+  const dataId = e.target.dataset.id;
+  console.log(`you pressed on the ${dataId}`);
 });
+
+// parent.addEventListener("click", () => {
+//   console.log("you pressed on the parent");
+// });
+//
+// child.addEventListener("click", (e) => {
+//   e.stopPropagation();
+//   console.log("you pressed on the child");
+// });
